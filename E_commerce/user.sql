c260900ce9f35e34fbbd595de3b6afc00a13059e
Muskan0266 <25muskaaan@gmail.com>
2026-03-14 12:53:23 +0530

    joins

--- E_commerce/user.sql
+++ E_commerce/user.sql
@@ -1,7 +1,7 @@
 CREATE DATABASE e_comm;
 USE e_comm;
 CREATE TABLE users(
-    id INT,
+    id INT PRIMARY KEY,
     name VARCHAR(50),
     email VARCHAR(100),
     phone VARCHAR(15),
@@ -38,6 +38,20 @@ VALUES (
     ),
     (5, 'Rosie', 'rosie12@gmail.com', 9875643987, 17),
     (6, 'Jenne', 'jenne12@gmail.com', 7865981234, 30);
+-- order table
+CREATE TABLE orders (
+    order_id INT PRIMARY KEY,
+    id INT,
+    product_name VARCHAR(100),
+    amount DECIMAL(10, 2),
+    order_date DATE,
+    FOREIGN KEY (id) REFERENCES users(id)
+);
+INSERT INTO orders (order_id, id, product_name, amount, order_date)
+VALUES (101, 1, 'Laptop', 55000.00, '2026-03-10'),
+    (102, 2, 'Headphones', 2000.00, '2026-03-11'),
+    (103, 1, 'Keyboard', 1500.00, '2026-03-12'),
+    (104, 3, 'Mouse', 800.00, '2026-03-13');
 SELECT name
 FROM users
 WHERE(age > 20);
@@ -49,3 +63,40 @@ ORDER BY age DESC;
 SELECT name
 FROM users
 LIMIT 2 OFFSET 3;
+-- update
+UPDATE users
+SET name = "Rista"
+WHERE age = 17
+select name
+from users;
+delete from users
+where id = 2;
+select *
+from users;
+alter table users
+add purchases int;
+alter table u
+    rename to users;
+alter table users drop age;
+drop table orders;
+drop table users;
+-- Joins
+SELECT *
+FROM users AS U
+    INNER JOIN orders AS O ON U.id = O.id;
+SELECT *
+FROM users AS U
+    RIGHT JOIN orders AS O ON U.id = O.order_id;
+SELECT *
+FROM users
+    LEFT JOIN orders ON users.id = orders.id;
+SELECT *
+FROM users
+    FULL JOIN orders ON users.id = orders.id;
+SELECT *
+FROM users
+    LEFT JOIN orders ON users.id = orders.id
+UNION
+SELECT *
+FROM users
+    RIGHT JOIN orders ON users.id = orders.id;
